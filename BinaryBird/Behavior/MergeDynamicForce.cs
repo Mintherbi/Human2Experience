ae28c1819ef3590be152b1b76c93095ae10156a4
jord97@korea.edu <jord97@korea.ac.kr>
2024-04-22 03:11:58 +0900

    Complete

--- BinaryBird/Behavior/MergeDynamicForce.cs
+++ BinaryBird/Behavior/MergeDynamicForce.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
 
 using BinaryBird.Data;
+using BinaryBird.Utilites;
+using Rhino.Render;
 
 namespace BinaryBird.Behavior
 {
-    public class MergeDynamicForce : GH_Component
+    public class MergeDynamicForce : GH_Component, IGH_VariableParameterComponent
     {
+        private GH_Document GrasshopperDocument;
+        private IGH_Component Component;
         /// <summary>
         /// Initializes a new instance of the MyComponent1 class.
         /// </summary>
@@ -18,14 +24,23 @@ namespace BinaryBird.Behavior
               "BinaryNature", "BinaryBird")
         {
         }
+        public override GH_Exposure Exposure
+        {
+            get
+            {
+                return GH_Exposure.septenary;
+            }
+        }
+        public override void CreateAttributes()
+        {
+            base.m_attributes = new CustomAttributes(this, 0);
+        }
 
         /// <summary>
         /// Registers all the input parameters for this component.
         /// </summary>
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
-            pManager.AddGenericParameter("Force", "F", "Dynamic List of Force", GH_ParamAccess.list);
-            pManager[0].DataMapping = GH_DataMapping.Flatten;
         }
 
         /// <summary>
@@ -45,19 +60,82 @@ namespace BinaryBird.Behavior
             List<IForce> Forces = new List<IForce>();
             List<IForce> temp = new List<IForce>();
 
-            while(DA.GetDataList(0,temp))
-            {
-                Forces.AddRange(temp);
-                temp.Clear();
-            }
+            Component = this;
+            int inputCount = Component.Params.Input.Count;
+            int hitCounter = 0; 
 
-            DA.SetDataList(0, Forces);
+            for (int i = 0; i < inputCount; i++)
+            {
+                IGH_DocumentObject connectedComponent = Component.Params.Input[i].Sources[0].Attributes.GetTopLevel.DocObject;
+                string name = connectedComponent.Name;
+                Component.Params.Input[i].NickName = name;
+
+                foreach (IGH_Goo a in Component.Params.Input[i].VolatileData.get_Branch(0))
+                {
+                    hitCounter++;
+                    IForce fd;
+                    bool worked = a.CastTo(out fd);
+                    if (!worked) { return; }
+                    Forces.Add(fd);
+                }
+            }
+            if (hitCounter > 0)
+            {
+                DA.SetDataList(0, Forces);
+            }
+        }
+        public bool CanInsertParameter(GH_ParameterSide side, int index)
+        {
+            if (side == GH_ParameterSide.Input)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool CanRemoveParameter(GH_ParameterSide side, int index)
+        {
+            if (side == GH_ParameterSide.Input && Params.Input.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public IGH_Param CreateParameter(GH_ParameterSide side, int index)
+        {
+            Param_GenericObject param = new Param_GenericObject();
+            param.Name = GH_ComponentParamServer.InventUniqueNickname("NewComponentMagicalName", Params.Input);
+            param.NickName = param.Name;
+            param.Description = "Param" + (Params.Input.Count + 1);
+            param.Access = GH_ParamAccess.item;
+
+            return param;
+        }
+        public bool DestroyParameter(GH_ParameterSide side, int index)
+        {
+            return true;
+        }
+        public void VariableParameterMaintenance()
+        {
+        }
+        private void ParamSourcesChanged(object sender, GH_ParamServerEventArgs e)
+        {
+            if (((e.ParameterSide == 0) && (e.ParameterIndex == (Component.Params.Input.Count - 1))) && (e.Parameter.SourceCount > 0))
+            {
+                IGH_Param param = this.CreateParameter(0, Component.Params.Input.Count);
+                Component.Params.RegisterInputParam(param);
+                Component.Params.OnParametersChanged();
+            }
         }
 
         /// <summary>
         /// Provides an Icon for the component.
-        /// </summary>
-/*       
+        /// </summary>  
         protected override System.Drawing.Bitmap Icon
         {
             get
@@ -67,7 +145,6 @@ namespace BinaryBird.Behavior
                 return null;
             }
         }
-*/
         /// <summary>
         /// Gets the unique ID for this component. Do not change this ID after release.
         /// </summary>
