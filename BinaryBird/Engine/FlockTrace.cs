ead3f319f56a253c1f8abfeec764aeb032a023e7
jord97@korea.edu <jord97@korea.ac.kr>
2024-04-22 15:47:45 +0900

    Spliting Engine and Trace

--- BinaryBird/Engine/FlockTrace.cs
+++ BinaryBird/Engine/FlockTrace.cs
@@ -11,7 +11,7 @@ using BinaryBird.Boid;
 
 namespace BinaryBird.Engine
 {
-    public class FlockEngine : GH_Component
+    public class FlockTrace : GH_Component
     {
         /// <summary>
         /// Each implementation of GH_Component must provide a public 
@@ -20,9 +20,9 @@ namespace BinaryBird.Engine
         /// Subcategory the panel. If you use non-existing tab or panel names, 
         /// new tabs/panels will automatically be created.
         /// </summary>
-        public FlockEngine()
-          : base("FlockEngine", "FE",
-            "Let's Fly!",
+        public FlockTrace()
+          : base("FlockTrace", "FT",
+            "The Trace of the Birds",
             "BinaryNature", "BinaryBird")
         {
         }
@@ -30,19 +30,20 @@ namespace BinaryBird.Engine
         /// <summary>
         /// Registers all the input parameters for this component.
         /// </summary>
-        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddPointParameter("Birds", "B", "Bird Seeking Freedom", GH_ParamAccess.list);
             pManager.AddGenericParameter("Force", "F", "Birds are seeking freedom but captured by unknown", GH_ParamAccess.list);
             pManager.AddGenericParameter("Behavior", "BH", "How the birds will fly?", GH_ParamAccess.item);
-            pManager.AddNumberParameter("Delta", "dt", "Time Step", GH_ParamAccess.item);
+            pManager.AddNumberParameter("Resolution", "R", "Length of the Step, Resolution of Trace", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Length", "L", "Length of the Trace", GH_ParamAccess.item);
             pManager.AddBooleanParameter("reset", "R", "You don`t like the move they make?", GH_ParamAccess.item);
         }
 
         /// <summary>
         /// Registers all the output parameters for this component.
         /// </summary>
-        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
             pManager.AddPointParameter("Trace", "T", "The history of flock", GH_ParamAccess.tree);
             pManager.AddIntegerParameter("delta", "dt", "Time Pass", GH_ParamAccess.item);
@@ -63,14 +64,16 @@ namespace BinaryBird.Engine
             List<Point3d> pt_bird = new List<Point3d>();
             List<IForce> Forces = new List<IForce>();
             BoidData Behavior = new BoidData();
-            double dt = new double();
+            double Resolution = new double();
+            int Length = new int();
             bool reset = new bool();
 
-            if (!DA.GetDataList(0, pt_bird)) { return; }
-            if (!DA.GetDataList(1, Forces)) { return; }
-            if (!DA.GetData(2, ref Behavior)) { return; }
-            if (!DA.GetData(3, ref dt)) { return; }
-            if (!DA.GetData(4, ref reset)) { return; }
+            if(!DA.GetDataList(0, pt_bird)) { return; }
+            if(!DA.GetDataList(1, Forces)) { return; }
+            if(!DA.GetData(2, ref Behavior)) { return; }
+            if(!DA.GetData(3, ref Resolution)) { return; }
+            if(!DA.GetData(4, ref Length)) { return; }
+            if(!DA.GetData(5, ref reset)) { return; }
             #endregion
 
             #region ///reset parameter
@@ -88,21 +91,24 @@ namespace BinaryBird.Engine
 
                     Trace.AddRange(subtree, path);
 
-                    Boid.Add(new Bird(pt_bird[a], new Vector3d(0, 0, 1), Behavior, Forces, dt));
+                    Boid.Add(new Bird(pt_bird[a], new Vector3d(0, 0, 1), Behavior, Forces, Resolution));
                 }
             }
+
             #endregion
-
-            for (int b = 0; b < Boid.Count; b++)
+            while (Length>0)
             {
-                Boid[b].Update(Boid);
-                Boid[b].ForceUpdate(Forces);
-                Boid[b].CheckSpeed();
-                Boid[b].Move();
-                Trace.Add(Boid[b].Location, new GH_Path(b));
+                for (int b = 0; b < Boid.Count; b++)
+                {
+                    Boid[b].Update(Boid);
+                    Boid[b].ForceUpdate(Forces);
+                    Boid[b].CheckSpeed();
+                    Boid[b].Move();
+                    Trace.Add(Boid[b].Location, new GH_Path(b));
+                }
+                Length--;
+                delta++;
             }
-
-            delta++;
 
             DA.SetDataTree(0, Trace);
             DA.SetData(1, delta);
@@ -115,7 +121,6 @@ namespace BinaryBird.Engine
         /// You can add image files to your project resources and access them like this:
         /// return Resources.IconForThisComponent;
         /// </summary>
-         /*
         protected override System.Drawing.Bitmap Icon
         {
             get
@@ -125,13 +130,15 @@ namespace BinaryBird.Engine
                 return null;
             }
         }
-        */
 
         /// <summary>
         /// Each component must have a unique Guid to identify it. 
         /// It is vital this Guid doesn't change otherwise old ghx files 
         /// that use the old ID will partially fail during loading.
         /// </summary>
-        public override Guid ComponentGuid => new Guid("c6a8cccc-4fa2-4127-98f9-ecfd8a9d242f");
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("FD8B8B03-5DBA-41ED-99D0-9F73CE6037EB"); }
+        }
     }
 }
